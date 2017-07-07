adf3e1d1962466699ab14d19cadbb3feffac7abe
Deniz Demir Gaidemak <dgaidemak@gmail.com>
2017-07-07 13:47:26 -0400

    Update Level Order Traversal.java

--- Data Structures/Trees/Level Order Traversal.java
+++ Data Structures/Trees/Level Order Traversal.java
@@ -5,6 +5,15 @@
        Node left;
        Node right;
    */
+  /*
+		Algorithm:
+			1) Create an empty Node queue
+			2) Create a temp node and assign root node to it
+			3) Loop through the temp node will be null
+				a. print temp data
+				b. enqueue temp's children from left to right to the queue
+				c. dequeue the beginning node (remember: queue is FIFO) and assign it to the temp node
+   */
    void levelOrder(Node root) {
       
        Queue<Node> queue = new LinkedList<Node>();
